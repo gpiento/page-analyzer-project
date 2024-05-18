21decb89ecb753e84c00df74e4b4f93062223e0a
gpiento <zzzest@gmail.com>
2024-05-19 01:41:13 +0300

    step 2

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -24,44 +24,4 @@ class AppTest {
             assertThat(response.code()).isEqualTo(200);
         });
     }
-
-    @Test
-    void testProductsPage() throws Exception {
-
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/products");
-            assertThat(response.code()).isEqualTo(200);
-        });
-    }
-
-    @Test
-    public void testProductPage() throws Exception {
-        var product = new Product("car", 100);
-        ProductsRepository.save(product);
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/products/" + product.getId());
-            assertThat(response.code()).isEqualTo(200);
-        });
-    }
-
-    @Test
-    public void testBuildProductPage() {
-        JavalinTest.test(app, (server, client) -> {
-            var response = client.get("/products/build");
-            assertThat(response.code()).isEqualTo(200);
-        });
-    }
-
-    @Test
-    public void testCreateProduct() throws Exception {
-        JavalinTest.test(app, (server, client) -> {
-            var requestBody = "title=car&price=100";
-            var response = client.post("/products", requestBody);
-            assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("car");
-        });
-
-        assertThat(ProductsRepository.getEntities()).hasSize(1);
-    }
-
 }
