47508b950b00e4992eee07a58d4bbaa9ad070a3c
gpiento <zzzest@gmail.com>
2024-05-19 01:34:34 +0300

    step 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,11 @@ import java.sql.SQLException;
 
 public final class App {
 
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
@@ -21,6 +26,6 @@ public final class App {
 
     public static void main(final String[] args) throws IOException, SQLException {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(getPort());
     }
 }
