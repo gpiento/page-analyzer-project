d3ab27012b193936482cea7b2a11d4f2fc57f9ea
gpiento <zzzest@gmail.com>
2024-06-18 01:15:08 +0300

    step 8

--- app/src/main/java/hexlet/code/repository/UrlsRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlsRepository.java
@@ -19,13 +19,14 @@ public class UrlsRepository extends BaseRepository {
 
     public static Url save(final Url url) throws SQLException {
 
-        String sql = "INSERT INTO urls (name) VALUES (?)";
+        String sql = "INSERT INTO URLS (NAME, CREATE_AT) VALUES (?, ?)";
 
         try (Connection conn = dataSource.getConnection();
              PreparedStatement preparedStatement = conn.prepareStatement(sql,
                      Statement.RETURN_GENERATED_KEYS)) {
 
             preparedStatement.setString(1, url.getName());
+            preparedStatement.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
             log.info("Executing save to UrlsRepository SQL: {}", sql);
             preparedStatement.executeUpdate();
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
